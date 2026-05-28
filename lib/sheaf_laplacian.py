6ef14b66ca484dd59cfefedca76b118df7905de4
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-05-28 20:49:00 +0000

    fixing sheaf laplacian

--- lib/sheaf_laplacian.py
+++ lib/sheaf_laplacian.py
@@ -1,23 +1,17 @@
 import torch
 import torch.nn.functional as F
 def sheaf_test(sheaf, edges, T):
-    E, 2, D, _ = sheaf.shape
+    E,_ , D, _ = sheaf.shape # E, 2, D, D: the two here is for both restriction maps; the first is for the map from node x_1 to e = (x_1, x_2), the other for x_2 to e = (x_1, x_2)
     # E, 2 = edges.shape
     block_rows = []
     for idx in range(T):
-        lx_idx = torch.zeros(D, D*T)
 
-        edges_mask = edges[:,0] == idx # (idx,n) such edges
-        edges_towards = torch.where(edges_mask.unsqueeze(-1), edges, -1)
-        edges_away = edges_towards.roll(1, 1)# if an edge (0,n) exists, then (n,0) must also exist in the list. makes sure these are aligned
-        edges_away_mask = edges[:,1] == idx
+        edges_mask = (edges[:,0] == idx)[:,None,None] # (idx,n) such edges
 
-        edges_mask = edges_mask[:, None, None]
-        edges_away_mask = edges_away_mask[:,None,None]
 
-        sheaves_transpose = torch.where(edges_mask, torch.transpose(sheaves, 1, 2), 0)
+        sheaves_transpose = torch.where(edges_mask, torch.transpose(sheaves[:,0,:,:], 1, 2), 0)
         pi_idx = F.pad(torch.eye(D), (idx*D,(T-idx-1)*D,0,0), 'constant', 0)
-        sheaves_L_1 = torch.where(edges_mask, sheaves, 0)
+        sheaves_L_1 = torch.where(edges_mask, sheaves[:,0,:,:], 0)
         L_1 = torch.einsum("exy,yz->exz",sheaves_L_1,pi_idx)
         
         ident = torch.eye(D*T).unsqueeze(0).repeat(E,1,1)
@@ -25,11 +19,12 @@ def sheaf_test(sheaf, edges, T):
         
         pi_n = torch.gather(ident,1,  index)
         
-        sheaves_L_2 = torch.where(edges_away_mask, sheaves, 0)
+        sheaves_L_2 = torch.where(edges_mask, sheaves[:,1,:,:], 0)
         L_2 = torch.einsum("exy,eyz->exz",sheaves_L_2,pi_n)
         
         lap_block_row = torch.einsum("exy,eyz->xz",sheaves_transpose,L_1-L_2)
         block_rows.append(lap_block_row)
+        print(idx)
         
         
      
@@ -95,16 +90,15 @@ if __name__ == "__main__":
 
     T = 2
     D = 3
-    E = 2*1 # since we're indexing the restriction map (0 <= (0,1)) we need ordered indices
 
-    edges = torch.tensor([[0,1],[0,1]], dtype=torch.int) # 2, 2
-    #edges = torch.cat([edges, edges.roll(1,1)], dim=0)
+    edges = torch.tensor([[0,1]], dtype=torch.int) # 1, 2
+    E = edges.shape[0]
     #edges = F.pad(edges, (0, E - edges.shape[0], 0, 0), "constant", -1) # E, 2
     
     
     
-    sheaves = torch.eye(D)[None,:,:].repeat(E,1,1) 
-    #sheaves = torch.rand(E ,D,D)
+    #sheaves = torch.eye(D)[None,:,:].repeat(E,1,1) 
+    sheaves = torch.rand(E ,2,D,D)
     paddings = torch.tensor([[True, True]])
     print(sheaves)
     print(edges)
