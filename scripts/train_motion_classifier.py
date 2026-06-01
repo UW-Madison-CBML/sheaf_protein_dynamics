cd6acc7f930ba368ac2cbcda7ee1ebf64043cc0c
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-06-01 18:01:07 +0000

    working on motion classifier

--- scripts/train_motion_classifier.py
+++ scripts/train_motion_classifier.py
@@ -1,7 +1,16 @@
 from lib import MotionClassifierDataset, SheafMotionClassifier
+import pandas as pd
+import numpy as np
+import torch
+import wandb
 
 def train_motion_classifier():
     # load in data
+    free_bound_csv = pd.
+    # set up validation split
+    # set up dataloader
+    # training  
+    # validation
     
 
 if __name__ == "__main__":
