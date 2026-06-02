29fcd7740a52c332c01fe4b51d1aeee11341a731
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-06-02 15:57:59 +0000

    working on train motion classifier

--- scripts/train_motion_classifier.py
+++ scripts/train_motion_classifier.py
@@ -12,8 +12,47 @@ import wandb
 from torch.utils.data import DataLoader
 
 import os
+
+class RunningStats:
+    def __init__(self):
+        self.n = 0
+        self.mean = 0.0
+        self.m2 = 0.0
+
+    def push(self, x):
+        self.n += 1
+        delta = x - self.mean
+        self.mean += delta / self.n
+        delta2 = x - self.mean
+        self.m2 += delta * delta2
+
+    @property
+    def variance(self):
+        return self.m2 / (self.n - 1) if self.n > 1 else 0.0
+
+    @property
+    def std_dev(self):
+        return math.sqrt(self.variance)
+
+def precision_recall_f1(gt_indices, pred_indices, num_classes):
+
+    # gt_indicies1: shape = (B), 0 <= min(), max() < num_classes
+    # pred_indicies2: shape = (B), 0 <= min(), max() < num_classes
+    # 0 <= i < num_classes
+    # returns: precision, recall, f1. shape = num_classes
+    confusion_mat = torch.einsum("bi, bj->ij", F.one_hot(pred_indices, num_classes=num_classes), F.one_hot(gt_indices, num_classes=num_classes))
+    diag = confusion_mat[torch.arange(num_classes),torch.arange(num_classes)]
+    recall = torch.nan_to_num(diag/confusion_mat.sum(dim=0), 0.0)
+    precision = torch.nan_to_num(diag/confusion_mat.sum(dim=1), 0.0)
+    f1 = torch.nan_to_num(2 * (precision * recall) / (precision + recall), 0.0)
+    return recall, precision, f1
+
+   
+
+
+
 def train_motion_classifier():
-    # hyperparameter
+    # hyperparameters
     epsilon = 5.0 # in Angstroms
     learning_rate = 1e-4
     epochs = 8
@@ -40,18 +79,19 @@ def train_motion_classifier():
     df = pd.concat([free_bound_df, dif_ligand_df], axis=0, ignore_index=True)
     # shuffle for randomness for now
     df = df.sample(frac=1, random_state=42).reset_index(drop=True) 
-    df_mask = df[df.index > int(df.index * 0.3)]
+    df_mask = df[df.index < int(len(df.index) * 0.3)]
     
     # set up validation split
     val_df = df[df_mask]
     df = df[~ df_mask]
     dataset = MotionClassifierDataset(df)
     val_dataset = MotionClassifierDataset(val_df)
+
     # set up dataloader
     loader = DataLoader(dataset,shuffle=True, batch_size=64, num_workers=16, collate_fn=MotionClassifierDataset.padd_collate, pin_memory=True, drop_last=False) 
     val_loader = DataLoader(val_dataset,shuffle=True, batch_size=64, num_workers=16, collate_fn=MotionClassifierDataset.padd_collate, pin_memory=True, drop_last=False) 
+
     #set up model
-    
     model = SheafMotionClassifier(len(MotionClassifierDataset.AMINO_ACIDS)+3, 8, num_classes=len(MotionClassifierDataset.MOTION_CLASSES))
     model = model.to(DEVICE)
     
@@ -60,9 +100,9 @@ def train_motion_classifier():
     crit = torch.nn.CrossEntropyLoss()
     
     # training  
-
     for epoch in range(epochs):
         for conformations1, conformations2, residues, motion_classes, lengths in loader:
+
             conformations1 = conformations1.to(DEVICE) # B, T, 3  
             conformations2 = conformations2.to(DEVICE) # B, T, 3 
             residues = residues.to(DEVICE) # B, T
@@ -73,19 +113,55 @@ def train_motion_classifier():
             node_features2 = torch.cat([conformations2, residues_one_hot],dim=2) 
             
             node_features = torch.stack([node_features1, node_features2], dim=1)
+            # just in case any previous operations have been calculating gradients
+            optimizer.zero_grad()
             logits = model(node_features, graphs, lengths, graph_paddings) # B 
 
             # compare prediction to ground truth classes
             loss = crit(logits, motion_classes)
-        
             run.log({"loss":loss.detach().cpu().item()})
+           
+            # back propagate and reset
+            loss.backward() 
+            optimizer.step()
+
         
         # validation
         model.eval()
+        precision_recall_f1 = [] 
         with torch.no_grad():
             for conformations1, conformations2, residues, motion_classes, lengths in val_loader:
-                run.log({"val_loss" : 0.0})
+                optimizer.zero_grad()
+
+                conformations1 = conformations1.to(DEVICE) # B, T, 3  
+                conformations2 = conformations2.to(DEVICE) # B, T, 3 
+                residues = residues.to(DEVICE) # B, T
+                motion_classes = motion_classes.to(DEVICE) # B
+                graphs, graph_paddings = build_graph(conformations1, conformations2, lengths, torch.tensor(epsilon, device=DEVICE)) # B, 2, E, 2
+                residues_one_hot = F.one_hot(residues, num_classes=len(MotionClassifierDataset.AMINO_ACIDS)) # B, T, amino_acids
+                node_features1 = torch.cat([conformations1, residues_one_hot],dim=2) # B, T, 3 + amino_acids 
+                node_features2 = torch.cat([conformations2, residues_one_hot],dim=2) 
+                
+                node_features = torch.stack([node_features1, node_features2], dim=1)
+                logits = model(node_features, graphs, lengths, graph_paddings) # B 
+
+                # compare prediction to ground truth classes
+                loss = crit(logits, motion_classes)
+                run.log({"val_loss":loss.cpu().item()})
+                preds = logits.cpu().argmax(dim=-1)
+                gt = motion_classes.cpu()
+                precision_recall_f1.append(torch.stack(precision_recall_f1(gt, preds, len(MotionClassifierDataset.MOTION_CLASSES)), dim=0))
+                
+        precision_recall_f1 = torch.stack(precision_recall_f1, dim=0) # len(val_loader), 3, num_classes
+        precision_recall_f1 = torch.stack([precision_recall_f1.mean(dim=0), precision_recall_f1.std(dim=0)],dim=1)
+        for i,agg in enumerate(["", "std"]):
+            for j, metric in enumerate(["precision","recall","f1"]):
+                for k, motion_class in enumerate(MotionClassifierDataset.MOTION_CLASSES):
+                    run.log({f"{motion_class}_{metric}_{agg}":precision_recall_f1(i,j,k)})
         
+                
+                
+
 
     
 
